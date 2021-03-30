627330e6e8ced251af1447c8c9cd4fe504dfc8b5
Matias Zapata <matiasignacio1998@gmail.com>
2021-03-29 23:36:32 -0300

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState([ 'Dragon Ball'])
+
+    // const handleAdd = () =>{
+    //     setCategories([...categories, 'Hunter X']);
+    //     setCategories( cats => [...cats,'Hunter X Hunter'] );
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr/>
+
+
+            <ol>
+                {
+                    categories.map( category =>(
+                        <GifGrid
+                            key={category}
+                             category= {category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
